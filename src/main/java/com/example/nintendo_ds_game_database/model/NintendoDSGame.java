2468acfcdd4e5ffcc17b0ff2960db48123b0fda8
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2023-12-28 21:18:45 +0600

    column name removed

--- src/main/java/com/example/nintendo_ds_game_database/model/NintendoDSGame.java
+++ src/main/java/com/example/nintendo_ds_game_database/model/NintendoDSGame.java
@@ -12,7 +12,6 @@ public class NintendoDSGame {
     private String title;
     private String developer;
     private String publisher;
-    @Column(name = "firstReleased")
     private Date firstReleased;
     private boolean JP;
     private boolean NA;
